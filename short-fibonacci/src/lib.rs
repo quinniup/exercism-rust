2f29c9c23bc304c5dedea2d75c8a623159bb5d73
cheneyin <howzequinn@gmail.com>
2022-10-31 20:59:31 +0800

    mod: refactor fibonacci sequence by 'match'.

--- short-fibonacci/src/lib.rs
+++ short-fibonacci/src/lib.rs
@@ -1,6 +1,6 @@
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    vec![]
+    create_buffer(0)
 }
 
 /// Create a buffer of `count` zeroes.
@@ -15,14 +15,21 @@ pub fn create_buffer(count: usize) -> Vec<u8> {
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    let mut v: Vec<u8> = vec![0; 5];
+    let mut v: Vec<u8> = create_buffer(5);
     for n in 0..5 {
-        if n == 0 || n == 1 {
-            v[n] = 1;
-        }
-        if n > 1 {
-            v[n] = v[n - 1] + v[n - 2];
+        match n {
+            0 | 1 => v[n] = 1,
+            _ => v[n] = v[n - 1] + v[n - 2]
         }
     }
     v
+    // for n in 0..5 {
+    //     if n == 0 || n == 1 {
+    //         v[n] = 1;
+    //     }
+    //     if n > 1 {
+    //         v[n] = v[n - 1] + v[n - 2]
+    //     }
+    // }
+    // v
 }
